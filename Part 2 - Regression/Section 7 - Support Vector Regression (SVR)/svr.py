3a472cd347956d97444a7a2d724f8048b7ee229e
Italo Fernandes on Ubuntu-Inspiron <italogsfernandes@gmail.com>
2018-01-09 13:43:51 -0200

    svr improvements

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -4,6 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -15,24 +17,23 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
+y = sc_y.fit_transform(y.reshape(len(y),1))
 
 # Fitting SVR to the dataset
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
+regressor = SVR(kernel='rbf')
 regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
-y_pred = sc_y.inverse_transform(y_pred)
+teste_employe = sc_X.transform(np.array([[6.5]]))
+teste_salary = regressor.predict(teste_employe)
 
 # Visualising the SVR results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X)), color = 'blue')
+plt.scatter(sc_X.inverse_transform(teste_employe), sc_X.inverse_transform(teste_salary), marker = 'o', color = 'green')
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
@@ -41,8 +42,9 @@ plt.show()
 # Visualising the SVR results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+plt.plot(sc_X.inverse_transform(X_grid), sc_y.inverse_transform(regressor.predict(X_grid)), color = 'blue')
+plt.scatter(sc_X.inverse_transform(teste_employe), sc_y.inverse_transform(teste_salary), marker = 'o', color = 'green')
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
