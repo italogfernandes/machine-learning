97c5df1becc6bd308f02f30d7ee9439078d7bd38
Italo Fernandes on Ubuntu-Inspiron <italogsfernandes@gmail.com>
2018-01-11 17:17:14 -0200

    pca improvements

--- Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
+++ Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Wine.csv')
-X = dataset.iloc[:, 0:13].values
-y = dataset.iloc[:, 13].values
+dataset = pd.read_csv('../datasets/Wine.csv')
+X = dataset.iloc[:, 0:-1].values
+y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -38,6 +38,14 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+  
+erros_up = cm[0,1] + cm[0,2] + cm[1,2]
+erros_down = cm[1,0] + cm[2,0] + cm[2,1]
+acertos = np.sum([cm[i,i] for i in range(len(cm))])
+total = np.sum(cm)
+accuracy = float(acertos)/total
+print(cm)
+print(accuracy)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -45,7 +53,7 @@ X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+             alpha = 0.2, cmap = ListedColormap(('red', 'green', 'blue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
@@ -63,7 +71,7 @@ X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+             alpha = 0.2, cmap = ListedColormap(('red', 'green', 'blue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
@@ -74,3 +82,59 @@ plt.xlabel('PC1')
 plt.ylabel('PC2')
 plt.legend()
 plt.show()
+
+# Visualising the Training set results
+"""
+from matplotlib.colors import ListedColormap
+plt.subplot(2,2,1)
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+predicted_grid = classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape)
+plt.contourf(X1, X2, predicted_grid,
+             alpha = 0.2, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+
+
+plt.subplot(2,2,2)
+X_set, y_set = X_train, y_train
+X3, X2 = np.meshgrid(np.arange(start = X_set[:, 2].min() - 1, stop = X_set[:, 2].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X3, X2, ,
+             alpha = 0.2, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X3.min(), X3.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 2], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC3')
+plt.ylabel('PC2')
+plt.legend()
+
+
+plt.subplot(2,2,3)
+X_set, y_set = X_train, y_train
+X1, X3 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 2].min() - 1, stop = X_set[:, 2].max() + 1, step = 0.01))
+#plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+#             alpha = 0.2, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X3.min(), X3.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 2],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC3')
+plt.legend()
+plt.show()
+"""
