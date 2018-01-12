aae59aaa452ad6d9a593f1a9d929b238cdd15fcc
Italo Fernandes on Ubuntu-Inspiron <italogsfernandes@gmail.com>
2018-01-12 16:15:50 -0200

    3d plot in pca

--- Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
+++ Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
@@ -5,11 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+
 dataset = pd.read_csv('../datasets/Wine.csv')
 X = dataset.iloc[:, 0:-1].values
 y = dataset.iloc[:, -1].values
 
+# Importing the dataset
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -22,7 +23,7 @@ X_test = sc.transform(X_test)
 
 # Applying PCA
 from sklearn.decomposition import PCA
-pca = PCA(n_components = 2)
+pca = PCA(n_components = 3)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 explained_variance = pca.explained_variance_ratio_
@@ -47,6 +48,46 @@ accuracy = float(acertos)/total
 print(cm)
 print(accuracy)
 
+
+
+# Visualising the Test set results 3 dimencoes
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import ListedColormap
+#ax.add_subplot(111,projection='3d')
+X_set, y_set = X_train, y_train
+axis_min = min([X_set[:, 0].min(), X_set[:, 1].min(), X_set[:, 2].min()])
+axis_max = max([X_set[:, 0].max(), X_set[:, 1].max(), X_set[:, 2].max()])
+main_grid = np.arange(axis_min,axis_max,0.2)
+[X1, X2, X3] = np.meshgrid(main_grid,main_grid,main_grid)
+
+predicted_grid = classifier.predict(np.array([X1.ravel(), X2.ravel(), X3.ravel()]).T).reshape(X1.shape)
+
+main_grid = np.arange(axis_min,axis_max,0.2)[:-1]
+[X1, X2, X3] = np.meshgrid(main_grid,main_grid,main_grid)
+
+a = np.diff(predicted_grid)
+
+fig = plt.figure()
+ax = Axes3D(fig)
+for n in range(len(a)-1):
+    for m in range(len(a)-1):
+        for o in range(len(a)-1):
+            if a[n,m,o] != 0:
+                ax.scatter(X1[n,m, o], X2[n,m, o], X3[n,m, o], c='black', alpha=0.2)
+
+#ax.contourf3D
+for i, j in enumerate(np.unique(y_set)):
+    ax.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], X_set[y_set == j, 2],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+ax.set_xlabel('PC1')
+ax.set_ylabel('PC2')
+ax.set_zlabel('PC3')
+ax.legend()
+plt.show()
+
+
+"""
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
@@ -65,7 +106,7 @@ plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
+# Visualising the Test set results 2 dimencoes
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -82,7 +123,7 @@ plt.xlabel('PC1')
 plt.ylabel('PC2')
 plt.legend()
 plt.show()
-
+"""
 # Visualising the Training set results
 """
 from matplotlib.colors import ListedColormap
